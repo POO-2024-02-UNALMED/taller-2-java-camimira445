6a41521da61a7c6cd76ee66f7dd18a590c34cb6e
camimira445 <jmirandaag@unal.edu.co>
2024-11-11 08:57:37 -0500

    Atributos

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,37 +1,14 @@
 package test;
-public class Auto {
+
+class Auto {
     String modelo;
     int precio;
-    Asiento[] asientos;
+    Asiento [] asientos;
     String marca;
     Motor motor;
     int registro;
-    static int cantidadCreados;
-
-    public int cantidadAsientos() {
-        int contador = 0;  
-
-        for (int i = 0; i < asientos.length; i++) {
-            if (asientos[i] != null) { 
-                contador++;
-            }
-        }
-
-        return contador;  
-    }
-
-    public boolean verificarIntegridad() {
+    int cantidadCreados;
     
-        if (motor == null || motor.registro != registro) {
-            return false;
-        }
 
-        for (Asiento asiento : asientos) {
-            if (asiento != null && asiento.registro != registro) {
-                return false;
-            }
-        }
-
-        return true; 
-    }
+    
 }
