0aaa2c112ae7b83b956b53e9d3335fea8cb7c3e1
camimira445 <jmirandaag@unal.edu.co>
2024-11-11 09:29:45 -0500

    111111

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -3,12 +3,37 @@ package test;
 class Auto {
     String modelo;
     int precio;
-    Asiento [] asientos;
+    Asiento[] asientos;
     String marca;
     Motor motor;
     int registro;
-    int cantidadCreados;
-    
+    static int cantidadCreados;
 
+    int cantidadAsientos(){
+        int numAsientos=0;
+        for(int i = 0; i <asientos.length ;i++){
+            if(asientos[i] !=null){
+                numAsientos++;
+            }
+        }
+        return numAsientos;
+    }
+
+    String verificarIntegridad(){
+        if(this.registro==this.motor.registro){
+            for(int i =0; i< this.asientos.length;i++){
+                if(asientos[i] !=null){
+                    if(asientos[i].registro != this.registro){
+                        return "“Las piezas no son originales";
+                    }
+                }
+            }
+            return "Auto original";
+        }
+        else{
+            return "Las piezas no son originales";
+        }
+
+    }
     
 }
