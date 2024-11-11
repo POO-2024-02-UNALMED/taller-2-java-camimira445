dad57492151796801bc19cbfcd51907cefe0d694
camimira445 <jmirandaag@unal.edu.co>
2024-11-10 23:09:48 -0500

    Ultimo por hoy

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,36 +1,37 @@
 package test;
-
 public class Auto {
     String modelo;
-	int precio;
-	Asiento[] asientos;
-	String Marca;
-	Motor motor;
-	int registro;
-	static int cantidadCreados;
-	
-   int cantidadAsientos(){
-	int num1=0;
-	for (int i =0; i<asientos.length;i++){
-       if(asientos[i]!=null){
-		  num1++;
-	    }
-	 } 
-	  return num1;
-  }
-     public String verificarIntegridad(){
-		if(this.registro==this.motor.registro){
-			for(int i = 0;i<asientos.length;i++){
-				if(asientos[i] != null){
-					if(asientos[i].registro != this.registro){
-						return "Las piezas no son originales";
-					}
-				}
-			}
-		    return "Auto original";
-		}else{
-			return "Las piezas no son originales";
-		}
-	  }
+    int precio;
+    Asiento[] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    public int cantidadAsientos() {
+        int contador = 0;  
+
+        for (int i = 0; i < asientos.length; i++) {
+            if (asientos[i] != null) { 
+                contador++;
+            }
+        }
+
+        return contador;  
+    }
+
+    public boolean verificarIntegridad() {
     
+        if (motor == null || motor.registro != registro) {
+            return false;
+        }
+
+        for (Asiento asiento : asientos) {
+            if (asiento != null && asiento.registro != registro) {
+                return false;
+            }
+        }
+
+        return true; 
+    }
 }
